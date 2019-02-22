46b3965ed2a0518d30cc0a7cd79c12f006cd3647
akunim <akunim.draws@gmail.com>
2019-02-22 16:10:31 +0000

    Inital commit

---
+++ bot.js
@@ -0,0 +1,87 @@
+const Discord = require('discord.js');
+const RichEmbed = require('discord.js').RichEmbed;
+const client = new Discord.Client();
+const rp = require('request-promise');
+
+const monitor = require("./feedMonitor.js");
+
+var secrets;
+
+try {
+  secrets = require(process.argv[2]);  
+}
+catch (e) {
+  throw "Bad secrets path, exiting...";
+}
+
+const BOT_KEY = secrets.BOT_KEY();
+var urlRegex = new RegExp(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi);
+var emojiRegex = new RegExp(/:([^\s]*):/gi);
+const COMMAND_SIGNS = ["!", "/", "."];
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  
+  // PASSIVE ACTIONS
+  console.log("Getting announcement channel...");
+  let announcementChannel = "";
+  client.guilds.forEach((guild) => {
+    guild.channels.forEach((channel) => {
+      if (channel.id === "304823513124962305"){
+        announcementChannel = channel;
+      }
+    })
+  })  
+  function comicMon() {
+    monitor.initCheckComicUpdate("http://latchkeykingdom.smackjeeves.com/rss/", announcementChannel)
+  }
+  function streamMon() {
+    monitor.initCheckStream("https://api.picarto.tv/v1/channel/name/LKComic", announcementChannel);
+  }
+  //304823513124962305 
+  setInterval(comicMon, 30000);
+  setInterval(streamMon, 30000);
+});
+
+client.on('message', msg => {
+  // DON'T TALK TO YOURSELF, DON'T RESPOND TO DMs
+  if (msg.author.id !== client.user.id && msg.guild !== null) {
+    
+    // BOB ACTIONS
+    if (msg.author.id === "87562842047279104") {
+      if (msg.content === 'ping') {
+        msg.channel.send("Pong");
+      }
+    }
+    
+    // COMMANDS
+    if(COMMAND_SIGNS.indexOf(msg.content.substring(0,1)) > -1){
+      var command = msg.content.split(" ")[0].substring(1);
+      var params = msg.content.split(" ");
+      var author = msg.author;
+      params.shift();
+      var commandRegex = new RegExp(/!+/gm);
+      
+      if (command !== "" && !command.match(commandRegex)){
+        //msg.channel.send("👏"); 
+      }
+      
+      if (command === "fm"){
+        if (params[0] == "info"){
+        }
+      }
+      if (command === "random"){
+        monitor.pollUrl("https://latchkeykingdom.smackjeeves.com/comics/random", (res) => {
+          msg.channel.send(res.headers.location);
+        });        
+      }
+    }
+  } // END don't talk to self
+});
+
+client.on('error', (err) => {
+  console.log("WEBSOCKET ERROR: " + err.message);
+});
+
+console.log("Loggin' in...");
+client.login(BOT_KEY);
---
+++ feedMonitor.js
@@ -0,0 +1,160 @@
+const RichEmbed = require("discord.js").RichEmbed;
+const Parser = require('rss-parser');
+const parser = new Parser();
+const https = require("https");
+
+var sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('feedMonitor.db');
+  try {
+    db.run("CREATE TABLE feeds (url TEXT, lastPoll TEXT)", [], (err) =>{
+      if (err){
+        //throw err;
+        console.log("Database already exists");
+      }
+    });  
+  }
+  catch (e){
+    console.log("Database exists -- probably");
+  }
+
+async function initCheckComicUpdate( url, alertChan ){
+  let query = 'SELECT lastPoll FROM feeds WHERE url like \'' + url + "'";
+  let result = await readDb( query );
+  let feed = await pollFeed( url );
+  if (result !== undefined || result !== "" || result.length > 1){
+    result.forEach((row) => {
+      let json = JSON.parse(row.lastPoll);
+      
+      if (feed.items[0].guid !== json.items[0].guid){
+        // Update detected
+        const embed = new RichEmbed()
+          .setTitle(feed.items[0].title)
+          .setColor(0xFF0000)
+          .setDescription(feed.items[0].pubDate + "\n" + feed.items[0].link)
+          .attachFiles(["./logo.webp"]);
+        alertChan.send(embed);
+        saveFeed( url, JSON.stringify(feed) );
+      }
+      
+    })    
+  }
+  else {
+    saveFeed( url, JSON.stringify(feed) );
+  }
+}
+
+async function initCheckStream( url, alertChan ){
+  let query = 'SELECT lastPoll FROM feeds WHERE url like \'' + url + "'";
+  let api = await pollApi( url );
+  let apiJson = JSON.parse(api);
+  if (apiJson.online){
+    let result = await readDb( query );
+
+    if (result !== undefined || result !== "" || result.length > 1){
+      result.forEach((row) => {
+        let json = JSON.parse(row.lastPoll);
+        // If the last stored value was 'offline', send an alert
+        if (!json.online){
+          const embed = new RichEmbed()
+            .setTitle(apiJson.name + " is streaming")
+            .setColor(0xFF0000)
+            .setDescription("https://picarto.tv/" + apiJson.name + " \n adult: " + apiJson.adult)
+            .attachFiles([apiJson.thumbnails.web]);
+          alertChan.send(embed);
+        }
+      })
+    }
+  }
+  saveFeed( url, api );
+}
+
+function readDb( query ){
+  return new Promise( resolve => {
+    db.all(query, [], (err, result) => {
+      resolve(result);
+    });    
+  })
+
+}
+
+function pollFeed( url ){
+  return new Promise(resolve => {
+    parser.parseURL(url, (err, feed) =>{
+      resolve(feed);      
+    });
+  })
+}
+
+function pollApi( url ) {
+  return new Promise(resolve => {
+    https.get(url, (resp) => {
+      let data = '';
+
+      // A chunk of data has been recieved.
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        resolve(data);
+      });
+
+      }).on("error", (err) => {
+        console.log("Error: " + err.message);
+      });
+  })
+}
+
+function pollUrlNoPromise( url, callback ){
+  https.get(url, (res) => {
+    let data = '';
+
+    // A chunk of data has been recieved.
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    res.on('end', () => {
+      callback(res);
+    });
+
+    }).on("error", (err) => {
+      return("Error: " + err.message);
+    });  
+}
+
+async function saveFeed( url, feedData ){
+  let db = new sqlite3.Database('feedMonitor.db');
+  let query = 'SELECT lastPoll FROM feeds WHERE url like \'' + url + "'";
+  db.all(query, [], (err, result) => {
+    // Update existing URL
+    if (result.length > 0){
+      let data = [feedData, url];
+      let q = "UPDATE feeds SET lastPoll = ? WHERE url = ?"
+      
+      db.run(q, data, (err)=> {
+        if (err){
+          console.log(err);
+        }
+      })
+    }
+    // Insert new URL and values
+    else {
+      let q = "INSERT INTO feeds VALUES (?,?)";
+
+      db.run(q, [url, JSON.stringify(feedData)], (err) => {
+        if (err){
+          console.log(err);
+        }
+      });      
+    }
+  })
+}
+
+module.exports = {
+  initCheckComicUpdate: initCheckComicUpdate,
+  initCheckStream: initCheckStream,
+  pollUrl: pollUrlNoPromise
+}
